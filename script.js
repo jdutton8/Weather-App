fa18ad84a874083c726bffaac009d724c4f33467
Jenny Dutton <jennydutton7@gmail.com>
2023-04-26 21:33:55 -0500

    added a search button and text area

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-var apiKey = "34ed152ea4eae279e0f5fd9e697df2a0";
+var apiKey = "cb0fb26fe05c92dfa0ba816b9520d228";
+
+var BASE_URL = 'http://api.openweathermap.org/data/2.5/forecast?q=towanda&appid=cb0fb26fe05c92dfa0ba816b9520d228';
+
+
+fetch(BASE_URL)
+    .then(function(res){
+        if(!res.ok) throw new Error('ooops');
+
+        return res.json();
+    })
+    .then(function(data){
+        console.log('data :>>', data);
+    })
+    .catch(function(error){
+        console.error(error);
+    });
 
