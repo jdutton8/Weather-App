cfe049725a65c591682ffcf56921648a54abf54f
Jenny Dutton <jennydutton7@gmail.com>
2023-04-27 12:42:37 -0500

    added html

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-var apiKey = "cb0fb26fe05c92dfa0ba816b9520d228";
+var apiKey = 'cb0fb26fe05c92dfa0ba816b9520d228';
 
-var BASE_URL = 'http://api.openweathermap.org/data/2.5/forecast?q=towanda&appid=cb0fb26fe05c92dfa0ba816b9520d228';
+var BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
 
+var city = 'Towanda';
 
-fetch(BASE_URL)
+fetch(BASE_URL + city + '&appid=' + apiKey)
     .then(function(res){
         if(!res.ok) throw new Error('ooops');
 
